ea5a56e11db91733279a6e503d97eeba5aed1766
Ikhsan N. Rosyidin <essanpupil@gmail.com>
2026-07-16 19:13:24 +0700

    Deploy instance profile for kibana and elasticsearch instances

--- us-east-1/vpc/elasticsearch-cluster.tf
+++ us-east-1/vpc/elasticsearch-cluster.tf
@@ -1,8 +1,33 @@
+data "aws_iam_policy_document" "elasticsearch" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "elasticsearch" {
+  name               = "es-role"
+  path               = "/"
+  assume_role_policy = data.aws_iam_policy_document.elasticsearch.json
+}
+
+resource "aws_iam_instance_profile" "elasticsearch" {
+  name = "es-nodes-profile"
+  role = aws_iam_role.elasticsearch.name
+}
+
 resource "aws_instance" "elasticsearch" {
   count                  = 3
   ami                    = "ami-0c7217cdde317cfec"
   instance_type          = "t2.micro"
   subnet_id              = aws_subnet.data.id
+  iam_instance_profile = aws_iam_instance_profile.elasticsearch.id
 
   tags = {
     Name = "es-node-${count.index + 1}"
--- us-east-1/vpc/kibana-logstash.tf
+++ us-east-1/vpc/kibana-logstash.tf
@@ -1,7 +1,32 @@
+data "aws_iam_policy_document" "kibana" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "kibana" {
+  name               = "kibana-role"
+  path               = "/"
+  assume_role_policy = data.aws_iam_policy_document.kibana.json
+}
+
+resource "aws_iam_instance_profile" "kibana" {
+  name = "kibana-profile"
+  role = aws_iam_role.kibana.name
+}
+
 resource "aws_instance" "kibana" {
   ami                    = "ami-0c7217cdde317cfec"
   instance_type          = "t2.micro"
   subnet_id              = aws_subnet.private.id
+  iam_instance_profile = aws_iam_instance_profile.kibana.id
 
   tags = {
     Name = "kibana"
