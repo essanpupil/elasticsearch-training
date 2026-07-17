1e937b75455d61c60e19de57d3eb054d7b72d443
Ikhsan N. Rosyidin <essanpupil@gmail.com>
2026-07-17 20:54:21 +0700

    remove unnecessary ssh ingress for bastion

--- us-east-2/ec2/bastion/bastion.tf
+++ us-east-2/ec2/bastion/bastion.tf
@@ -8,17 +8,6 @@ resource "aws_security_group" "bastion_sg" {
   }
 }
 
-resource "aws_vpc_security_group_ingress_rule" "allow_ssh_from_self" {
-  security_group_id = aws_security_group.bastion_sg.id
-  description       = "Allow SSH from same security group across any subnet"
-
-  referenced_security_group_id = aws_security_group.bastion_sg.id
-
-  from_port   = 22
-  to_port     = 22
-  ip_protocol = "tcp"
-}
-
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
   security_group_id = aws_security_group.bastion_sg.id
   description       = "Allow all outbound traffic"
