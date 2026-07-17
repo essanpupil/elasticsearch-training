c66cedfdc4a084d70bf15ec4f6de33df760ec7aa
Ikhsan N. Rosyidin <essanpupil@gmail.com>
2026-07-17 17:15:56 +0700

    Deploy elasticsearch nodes

---
+++ us-east-2/ec2/elasticsearch/data.tf
@@ -0,0 +1,19 @@
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+
+  config = {
+    bucket = "terraform-state-803309190098"
+    key    = "us-east-2/vpc/terraform.tfstate"
+    region = "us-east-2"
+  }
+}
+
+data "terraform_remote_state" "bastion" {
+  backend = "s3"
+
+  config = {
+    bucket = "terraform-state-803309190098"
+    key    = "us-east-2/ec2/bastion/terraform.tfstate"
+    region = "us-east-2"
+  }
+}
