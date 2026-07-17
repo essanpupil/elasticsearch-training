12df011884073b8da4114e3ee42a7f75a07eee02
Ikhsan N. Rosyidin <essanpupil@gmail.com>
2026-07-17 23:26:51 +0700

    remove unused config

--- us-east-1/vpc/data.tf
+++
@@ -1,9 +0,0 @@
-data "aws_ami" "fck_nat" {
-  most_recent = true
-  owners      = ["568608671756"] # Official fck-nat AWS account ID
-
-  filter {
-    name   = "name"
-    values = ["fck-nat-al2023-*-arm64-ebs"]
-  }
-}
