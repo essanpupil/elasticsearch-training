a8743bf64ae5b6efa05cc7f9f25a1ef18d7cccdd
Ikhsan N. Rosyidin <essanpupil@gmail.com>
2026-07-17 16:14:49 +0700

    copy existing terraform state to S3 bucket after creation of terraform state bucket

--- us-east-2/s3/terraform-state/terraform.tf
+++ us-east-2/s3/terraform-state/terraform.tf
@@ -1,4 +1,10 @@
 terraform {
+  backend "s3" {
+    bucket         = "terraform-state-803309190098"
+    key            = "us-east-2/s3/terraform-state"
+    region         = "us-east-2"
+    encrypt        = true
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
